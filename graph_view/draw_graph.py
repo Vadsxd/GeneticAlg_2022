a4f997b63994d32338b09bd83f2e31f2fa43131a
vasiliy2002 <vasiliyshangichev@mail.ru>
2022-07-09 18:34:28 +0300

    test

--- graph_view/draw_graph.py
+++ graph_view/draw_graph.py
@@ -1,6 +1,6 @@
 import graphviz
 from typing import List
-from graph_view.png_func import create_empty_png_file
+from png_func import create_empty_png_file
 import os
 
 def graph_to_png(graph_list: List, names_vertexes, out_path: str):
@@ -26,6 +26,43 @@ def graph_to_png(graph_list: List, names_vertexes, out_path: str):
     path_without_ext, _ = os.path.splitext(out_path)
 
     os.remove(path_without_ext + ".gv")
+
+def individual_to_png(individual, graph_list: List, names_vertexes, out_path: str):
+    # если нечего рисовать
+    if graph_list == []:
+        create_empty_png_file(out_path)
+        return
+
+    view_graph = graphviz.Graph(strict=True)
+    view_graph.attr('node', shape='circle')
+
+    # создаем вершины
+    for vert_ind in range(len(graph_list)):
+        view_graph.node(str(names_vertexes[vert_ind]))
+
+    indiv_graph = individual.graph
+
+    # создаем ребра
+    for vert1_ind, list_edges in enumerate(graph_list):
+        for edges, vert2_ind in list_edges:
+            edge_color = 'grey'
+            if (edges, vert2_ind) in indiv_graph[vert1_ind]:
+                edge_color = 'black'
+            view_graph.edge(str(names_vertexes[vert1_ind]), str(names_vertexes[vert2_ind]), label=str(edges), color=edge_color)
+
+    view_graph.render(engine='fdp', outfile=out_path)
+
+    path_without_ext, _ = os.path.splitext(out_path)
+
+    os.remove(path_without_ext + ".gv")
+
+
+
+
+
+
+
+
 
 
 
--- graph_view/example.py
+++ graph_view/example.py
@@ -1,11 +1,26 @@
 from genetic_algorithm.src.graph_functions import create_full_graph
 from draw_graph import graph_to_png
 from windows.create_graph_win.graph_func import graph_to_str
+from genetic_algorithm.src.individual import Individual
+from genetic_algorithm.src.ga_functions import create_individuals
+import random
+import sys
+
+
+
 
 n = 10
 graph = create_full_graph(n)
+Individual._len_gen_code = (n * (n-1)) // 2
+Individual._initial_graph = graph
+individuals = create_individuals(Individual._len_gen_code, 10)
 names_vert = [i for i in range(1, n + 1)]
-graph_to_png(graph, names_vert, out_path='graph.png')
+#graph_to_png(graph, names_vert, out_path='graph.png')
+
+names_vert = list(range(n))
+random.shuffle(names_vert)
+individual_to_png(individuals[0], graph, names_vert, out_path="indiv_graph.png")
+
 
 #names = [i for i in range(n)]
 #print(graph_to_str(graph, names))
