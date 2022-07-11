376695ab0181dfe774f1a17043da381ad3dd3d43
Maxim Azarov <az.maks.work@mail.ru>
2022-07-11 22:35:01 +0300

    icon

--- GUI.py
+++ GUI.py
@@ -3,8 +3,9 @@ import windows.primary_win.primary_window as primary_window
 from windows.handler_exit_program import handler_exit_program
 
 dpg.create_context()
-
 dpg.create_viewport(title='Genetic Program', width=620, height=350, resizable=False)
+dpg.set_viewport_large_icon("icon.ico")
+dpg.set_viewport_small_icon("icon.ico")
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_exit_callback(handler_exit_program)
